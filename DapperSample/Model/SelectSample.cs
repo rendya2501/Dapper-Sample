79d48c55e7de396a918e8c3c9fc396775b59a189
伊藤 駿 <rendya2501@gmail.com>
2022-07-29 23:03:53 +0900

    Query、QueryFirstOrDefault、ExecuteScalar検証

---
+++ DapperSample/Model/SelectSample.cs
@@ -0,0 +1,87 @@
+﻿using Dapper;
+using DapperSample.Entity;
+using System;
+using System.Data.SqlClient;
+
+
+namespace DapperSample.Model
+{
+    internal class SelectSample
+    {
+        /// <summary>
+        /// 接続情報
+        /// </summary>
+        private readonly SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+        {
+            DataSource = @"(LocalDB)\MSSQLLocalDB",
+            AttachDBFilename = System.IO.Path.GetFullPath(@"..\..\Database1.mdf"),
+            IntegratedSecurity = true,
+        };
+
+        public void Execute()
+        {
+            Query();
+            QueryFirstOrDefault();
+            ExecuteScalar();
+        }
+
+
+        private void Query()
+        {
+            using (var connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+
+                // IEnumerable型が返る。
+                string sql = "SELECT * FROM Person";
+                var person = connection.Query<Person>(sql);
+                foreach (var item in person)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+
+                // 結果がない場合、空のIEnumerableが返る。
+                string sql2 = "SELECT * FROM Person WHERE ID = @ID";
+                var person2 = connection.Query<Person>(sql2, new { ID = 4 });
+                foreach (var item in person2)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+        }
+
+        private void QueryFirstOrDefault()
+        {
+            using (var connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+
+                // 1件のレコードが返る。
+                string sql = "SELECT * FROM Person WHERE ID = @ID";
+                var person = connection.QueryFirstOrDefault<Person>(sql,new { ID = 1});
+                Console.WriteLine(person.ID + ":" + person.Name);
+
+                // 1件もない場合はnullとなる
+                var person2 = connection.QueryFirstOrDefault<Person>(sql, new { ID = 4 });
+                Console.WriteLine(person2.ID + ":" + person2.Name);
+            }
+        }
+
+        private void ExecuteScalar()
+        {
+            using (var connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+
+                // 単一レコードの特定の列を取得する
+                string sql = "SELECT [Name] FROM [Person] WHERE [ID] = @ID";
+                var name = connection.ExecuteScalar<string>(sql, new { ID = 1 });
+                Console.WriteLine(name);
+
+                // 1件もない場合はnullとなる
+                var name2 = connection.ExecuteScalar<string>(sql, new { ID = 4 });
+                Console.WriteLine(name2);
+            }
+        }
+    }
+}
--- DapperSample/Program.cs
+++ DapperSample/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DapperSample.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,8 @@ namespace DapperSample
             new BasicSelectSample().Execute();
 
             new CRUDSample().Execute();
+
+            new SelectSample().Execute();
         }
     }
 }
